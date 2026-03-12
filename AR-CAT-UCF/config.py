61db6c95dd0f41c161a0153e324427e8b2f40112
divinefoxx <divine.foxx0@gmail.com>
2026-03-12 19:16:51 -0400

    Finished config class, is now ready for use.

--- AR-CAT-UCF/config.py
+++ AR-CAT-UCF/config.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import os
 from dotenv import load_dotenv
+from configuration import settings
 
 @dataclass(frozen=True)
 class Config:
@@ -14,9 +15,33 @@ class Config:
     counties_to_watch: dict[str]
     ping_roles: dict[str, str]
     ping_alerts: dict[str]
+    buffer: int
     
     @staticmethod
     def load() -> "Config":
         token = os.getenv("API-TOKEN")
-        guild_id = 300
-
+        guild_id = settings.guild_id
+        contact_header = os.getenv('HEADER')
+        cycle_time = settings.cycleTime
+        version_id = settings.version
+        alert_colors = settings.polygon_colors_SAME
+        channels = settings.channels
+        ping_roles = settings.pings
+        ping_alerts = settings.alertCodes
+        counties_to_watch = settings.countiesToMonitor
+        buffer = settings.bufferMiles
+        
+        return Config(
+            token=token,
+            guild_id=guild_id,
+            contact_header=contact_header,
+            channels=channels,
+            cycle_time=cycle_time,
+            version_id=version_id,
+            alert_colors=alert_colors,
+            counties_to_watch=counties_to_watch,
+            ping_roles=ping_roles,
+            ping_alerts=ping_alerts,
+            buffer=buffer
+        )
+        
