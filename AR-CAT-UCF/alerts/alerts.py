1dde0b78ce6ea09f0e84433356ba0ad89558775a
divinefoxx <divine.foxx0@gmail.com>
2026-03-25 10:53:18 -0400

    Started work on alerts.py

--- AR-CAT-UCF/alerts/alerts.py
+++ AR-CAT-UCF/alerts/alerts.py
@@ -7,3 +7,23 @@ from geometry import zones
 storageTime = config.storage_time
 polygonColors = config.alert_colors
 
+IGNORE_LIST = [
+    "TOR",
+    "SVR",
+    "FFW",
+    "SVS",
+    "SPS",
+    "FFS",
+]
+
+class Alerts():
+    def __init__(self):
+        self.request_header = config.contact_header
+        self.initialized = True
+        self.ActiveAlerts = {}
+        log.info("Alerts have been initialized")
+        
+    def cycle(self) -> dict:
+        log.info("Running cycle")
+        
+        
