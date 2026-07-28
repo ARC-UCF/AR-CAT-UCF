8c66039e7b89d0aa22bba3591b41c1f193af55da
divinefoxx <divine.foxx0@gmail.com>
2026-07-28 10:26:57 -0400

    Finalized for testing.

--- AR-CAT-UCF/main.py
+++ AR-CAT-UCF/main.py
@@ -1,12 +1,9 @@
 from bot import login
 from logging.syslogger import log
 
-class Controller():
-    def __init__(self):
-        log.info(f"Controller initialized.")
-        
 
 def run():
     login()
     
+run()
     
